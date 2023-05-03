c7effb16a967cc1f9c380a209fb271da8ee4d81f
Daniel Sammons <daniel@ip-172-31-20-193.ec2.internal>
2023-05-03 17:25:01 +0000

    woops

--- dotfiles/common_profile.sh
+++ dotfiles/common_profile.sh
@@ -1,6 +1,6 @@
 export DEV_HOME="$HOME/software"
 
 source ~/.git-prompt.sh
-PS1='$(__git_ps1 " (%s)")\$'
+PS1='\w$(__git_ps1 " (%s)")\$'
 # Tab completion for branch names
 source ~/.git-completion.bash
